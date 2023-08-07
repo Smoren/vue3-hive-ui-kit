15b6a7b143dcce6c7d9322c6519d74fcb36fdafe
Garrison <garrison@pc103895.PKRVMN.local>
2023-08-07 11:21:17 +0300

    dropdown: fix async load options

--- src/common/types/select.ts
+++ src/common/types/select.ts
@@ -1,4 +1,9 @@
+import { ComputedRef } from 'vue';
+
 export type Value = string | number | null;
+
 export type Option = {
   [key: string]: Value
 };
+
+export type OptionsRef = ComputedRef<Option[] | undefined>
