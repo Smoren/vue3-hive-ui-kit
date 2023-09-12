02ae75c05db0346f0da31147e6e9ff0b93819916
Garrison <garrison@pc103895.PKRVMN.local>
2023-09-12 15:51:06 +0300

    badge: create component

---
+++ src/components/hive-badge/hive-badge.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+type Counter = {
+  count: number;
+  mode: number;
+};
+
+const props = defineProps<{
+  counter: Counter | number;
+}>();
+
+const isNum = typeof props.counter === 'number';
+const countObj = props.counter as Counter;
+</script>
+
+<template>
+  <div v-if="isNum && counter" class="badge">
+    {{ counter }}
+  </div>
+  <div v-else-if="countObj.count" class="badge" :class="{ pulse: countObj.mode === 1 }">
+    {{ countObj.count }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/variables.scss';
+
+.badge {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  padding: 3px;
+  border: none;
+  border-radius: 50%;
+  color: $secondary;
+  background-color: $accent-red;
+  font-size: 16px;
+  line-height: 1;
+  z-index: 3;
+  font-weight: 600;
+}
+
+.pulse {
+  animation: pulse-note 2500ms linear infinite;
+}
+
+@keyframes pulse-note {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import HiveBadge from '@/components/hive-badge/hive-badge.vue';
 import HiveButton from '@/components/hive-button/hive-button.vue';
 import HiveDialog from '@/components/hive-dialog/hive-dialog.vue';
 import HiveDropDown from '@/components/hive-drop-down/hive-drop-down.vue';
@@ -6,4 +7,4 @@ import HiveInput from '@/components/hive-input/hive-input.vue';
 import HiveLoader from '@/components/hive-loader/hive-loader.vue';
 import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
 
-export { HiveButton, HiveDialog, HiveDropDown, HiveFilter, HiveInput, HiveLoader, HiveTextarea };
+export { HiveBadge, HiveButton, HiveDialog, HiveDropDown, HiveFilter, HiveInput, HiveLoader, HiveTextarea };
