f44e9b7c13961b7b40360702a343d7ad71e39366
Garrison <garrison@pc103895.PKRVMN.local>
2023-09-12 11:32:43 +0300

    filter: add in file-exporte

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import HiveButton from '@/components/hive-button/hive-button.vue';
 import HiveDialog from '@/components/hive-dialog/hive-dialog.vue';
 import HiveDropDown from '@/components/hive-drop-down/hive-drop-down.vue';
+import HiveFilter from './components/hive-filter/HiveFilter.vue';
 import HiveInput from '@/components/hive-input/hive-input.vue';
 import HiveLoader from '@/components/hive-loader/hive-loader.vue';
 import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
 
-export { HiveButton, HiveDialog, HiveDropDown, HiveInput, HiveLoader, HiveTextarea };
+export { HiveButton, HiveDialog, HiveDropDown, HiveFilter, HiveInput, HiveLoader, HiveTextarea };
