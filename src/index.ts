1f91b61bb7eb64fe76735fe8979441c7c7c19fa1
Anton Lvov <lvov.anton.02@gmail.com>
2023-09-27 14:36:44 +0300

    Index file fix

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,25 @@ import HiveBadge from '@/components/hive-badge/hive-badge.vue';
 import HiveButton from '@/components/hive-button/hive-button.vue';
 import HiveDialog from '@/components/hive-dialog/hive-dialog.vue';
 import HiveDropDown from '@/components/hive-drop-down/hive-drop-down.vue';
-import HiveFilter from './components/hive-input-search/hive-input-search.vue';
+import HiveInputSearch from './components/hive-input-search/hive-input-search.vue';
 import HiveCheckbox from './components/hive-checkbox/hive-checkbox.vue';
 import HiveCheckboxGroup from './components/hive-checkbox-group/hive-checkbox-group.vue';
 import HiveInput from '@/components/hive-input/hive-input.vue';
-import HiveLoader from '@/components/hive-preloader/hive-preloader.vue';
+import HivePreloader from '@/components/hive-preloader/hive-preloader.vue';
 import HiveRadioGroup from '@/components/hive-radio-group/hive-radio-group.vue';
 import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
+import HiveMultiselect from '@/components/hive-multiselect/hive-multiselect.vue';
 
 export {
   HiveBadge,
   HiveButton,
   HiveDialog,
   HiveDropDown,
-  HiveFilter,
+  HiveInputSearch,
   HiveCheckbox,
   HiveCheckboxGroup,
   HiveInput,
-  HiveLoader,
+  HivePreloader,
   HiveMultiselect,
   HiveRadioGroup,
   HiveTextarea,
