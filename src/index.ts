715e4448c14894a2d7f93306c4126a608d71a79d
Anton Lvov <lvov.anton.02@gmail.com>
2023-09-27 14:33:50 +0300

    Raname elements

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import HiveBadge from '@/components/hive-badge/hive-badge.vue';
 import HiveButton from '@/components/hive-button/hive-button.vue';
 import HiveDialog from '@/components/hive-dialog/hive-dialog.vue';
 import HiveDropDown from '@/components/hive-drop-down/hive-drop-down.vue';
-import HiveFilter from '@/components/hive-filter/hive-filter.vue';
-import HiveCheckbox from '@/components/hive-checkbox/hive-checkbox.vue';
-import HiveCheckboxGroup from '@/components/hive-checkbox-group/hive-checkbox-group.vue';
+import HiveFilter from './components/hive-input-search/hive-input-search.vue';
+import HiveCheckbox from './components/hive-checkbox/hive-checkbox.vue';
+import HiveCheckboxGroup from './components/hive-checkbox-group/hive-checkbox-group.vue';
 import HiveInput from '@/components/hive-input/hive-input.vue';
-import HiveLoader from '@/components/hive-loader/hive-loader.vue';
-import HiveMultiselect from '@/components/hive-multiselect/hive-multiselect.vue'
+import HiveLoader from '@/components/hive-preloader/hive-preloader.vue';
 import HiveRadioGroup from '@/components/hive-radio-group/hive-radio-group.vue';
 import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
 
