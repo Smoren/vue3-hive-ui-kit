a4ba5f0f5782cbbb3e0a3183b839cc2a5b19bb5d
Garrison <garrison@pc103895.PKRVMN.local>
2023-10-19 14:21:35 +0300

    build: update export components

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,45 @@
+import HiveAutocomplete from '@/components/hive-autocomplete/hive-autocomplete.vue';
 import HiveBadge from '@/components/hive-badge/hive-badge.vue';
 import HiveButton from '@/components/hive-button/hive-button.vue';
+import HiveCheckbox from '@/components/hive-checkbox/hive-checkbox.vue';
+import HiveCheckboxGroup from '@/components/hive-checkbox-group/hive-checkbox-group.vue';
 import HiveDialog from '@/components/hive-dialog/hive-dialog.vue';
 import HiveDropDown from '@/components/hive-drop-down/hive-drop-down.vue';
-import HiveInputSearch from './components/hive-input-search/hive-input-search.vue';
-import HiveCheckbox from './components/hive-checkbox/hive-checkbox.vue';
-import HiveCheckboxGroup from './components/hive-checkbox-group/hive-checkbox-group.vue';
+import HiveHtmlEditor from '@/components/hive-html-editor/hive-html-editor.vue';
 import HiveInput from '@/components/hive-input/hive-input.vue';
+import HiveInputSearch from '@/components/hive-input-search/hive-input-search.vue';
+import HiveListLoader from '@/components/hive-list-loader/hive-list-loader.vue'; 
 import HiveLoader from '@/components/hive-loader/hive-loader.vue';
-import HiveRadioGroup from '@/components/hive-radio-group/hive-radio-group.vue';
-import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
+import HiveMultiautocomplete from '@/components/hive-multiautocomplete/hive-multiautocomplete.vue';
 import HiveMultiselect from '@/components/hive-multiselect/hive-multiselect.vue';
+import HiveRadioGroup from '@/components/hive-radio-group/hive-radio-group.vue';
+import HiveSkeleton from '@/components/hive-skeleton/hive-skeleton.vue';
+import HiveSplitter from '@/components/hive-splitter/hive-splitter.vue';
+import HiveTabGroup from '@/components/hive-tab-group/hive-tab-group.vue';
+import HiveTab from '@/components/hive-tab-group/hive-tab.vue';
+import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
+import HiveUploadFile from '@/components/hive-upload-file/hive-upload-file.vue';
 
 export {
+  HiveAutocomplete,
   HiveBadge,
   HiveButton,
   HiveDialog,
   HiveDropDown,
-  HiveInputSearch,
-  HiveCheckbox,
-  HiveCheckboxGroup,
+  HiveHtmlEditor,
   HiveInput,
+  HiveInputSearch,
+  HiveListLoader,
   HiveLoader,
+  HiveMultiautocomplete,
   HiveMultiselect,
   HiveRadioGroup,
+  HiveCheckbox,
+  HiveCheckboxGroup,
+  HiveSkeleton,
+  HiveSplitter,
+  HiveTabGroup,
+  HiveTab,
   HiveTextarea,
+  HiveUploadFile,
 };
