feb68ab2f40154f80170f97cb3dff2b1f5844960
Anton Lvov <lvov.anton.02@gmail.com>
2024-03-19 17:32:20 +0300

    fix

--- src/examples/components/filter-new.vue
+++ src/examples/components/filter-new.vue
@@ -45,7 +45,7 @@ const roundTwoDigits = (value: number) => {
   return +value.toFixed(2);
 };
 
-const firstMatrix = variant.matrix_5;
+let variantMatrix = variant.matrix_4;
 
 const calculatePriorityElement = (arr: number[]) =>
   roundTwoDigits(
@@ -57,16 +57,10 @@ const calculatePriorityElement = (arr: number[]) =>
 
 const calculatePriorityVector = (matrix: number[][]) => matrix.map((arr) => calculatePriorityElement(arr));
 
-const priorityVector = calculatePriorityVector(firstMatrix);
-
-const priorityVectorSum = priorityVector.reduce((acc, value) => acc + value, 0);
-
 const calculateWeightElement = (vectorElement: number, vectorSum: number) => roundTwoDigits(vectorElement / vectorSum);
 
 const calculateWeightVector = (vector: number[]) =>
   vector.map((element) => calculateWeightElement(element, priorityVectorSum));
-
-const weightVector = calculateWeightVector(priorityVector);
 
 const calculateMatrixWeightElement = (matrixRow: number[], weightVector: number[]) =>
   roundTwoDigits(matrixRow.reduce((acc, value, index) => acc + value * weightVector[index]!, 0));
@@ -74,26 +68,73 @@ const calculateMatrixWeightElement = (matrixRow: number[], weightVector: number[
 const calculateMatrixWeightVector = (matrix: number[][]) =>
   matrix.map((matrixRow) => calculateMatrixWeightElement(matrixRow, weightVector));
 
-const matrixWeightVector = calculateMatrixWeightVector(firstMatrix);
-
 const calculateLambdaVector = (weightVector: number[], matrixWeightVector: number[]) =>
   matrixWeightVector.map((value, index) => roundTwoDigits(value / weightVector[index]!));
 
-const lambdaVector = calculateLambdaVector(weightVector, matrixWeightVector);
-
 const calculateEigenvalue = (lambdaVector: number[]) => Math.max(...lambdaVector);
-
-const eigenvalue = calculateEigenvalue(lambdaVector);
 
 const calculateConsistencyIndex = (eigenvalue: number, lambdaVector: number[]) =>
   roundTwoDigits((eigenvalue - lambdaVector.length) / (lambdaVector.length - 1));
 
-const consistencyIndex = calculateConsistencyIndex(eigenvalue, lambdaVector);
-
 const calculateConsistencyRatio = (matrix: number[][], consistencyIndex: number) =>
   roundTwoDigits(consistencyIndex / mathExpectationsOfConsistencyIndex[matrix.length]!);
 
-const consistencyRatio = calculateConsistencyRatio(firstMatrix, consistencyIndex);
+const isConsistencyAcceptable = (consistencyRatio: number) => Math.abs(consistencyRatio) < 0.1;
+
+const calculateWeightsRatioMatrix = (weightVector: number[]) => {
+  const matrix: number[][] = [];
+  weightVector.forEach((weight) => {
+    matrix.push(weightVector.map((value) => roundTwoDigits(weight / value)));
+  });
+
+  return matrix;
+};
+
+const calculateCorrectedWeightRatioMatrix = (matrix: number[][], weightsRatioMatrix: number[][]) => {
+  const newMatrix = matrix.map((arr, index) =>
+    arr.map((value, index2) => Math.abs(roundTwoDigits(value - weightsRatioMatrix[index]![index2]!))),
+  );
+  return newMatrix;
+};
+
+const findMaxSumIndex = (weightRatioMatrix: number[][]) => {
+  const sums = weightRatioMatrix.map((arr) => arr.reduce((acc, value) => acc + value, 0));
+  return sums.indexOf(Math.max(...sums));
+};
+
+const calculateNewCorrectedMatrix = (matrix: number[][], weightRatioMatrix: number[][], indexToChange: number) =>
+  matrix.map((arr, index) => {
+    if (index === indexToChange) return weightRatioMatrix[index]!;
+    return arr.map((value, index2) =>
+      index2 === indexToChange ? roundTwoDigits(1 / weightRatioMatrix[indexToChange]![index]!) : value,
+    );
+  });
+
+let priorityVector = calculatePriorityVector(variantMatrix);
+let priorityVectorSum = priorityVector.reduce((acc, value) => acc + value, 0);
+let weightVector = calculateWeightVector(priorityVector);
+let matrixWeightVector = calculateMatrixWeightVector(variantMatrix);
+let lambdaVector = calculateLambdaVector(weightVector, matrixWeightVector);
+let eigenvalue = calculateEigenvalue(lambdaVector);
+let consistencyIndex = calculateConsistencyIndex(eigenvalue, lambdaVector);
+let consistencyRatio = calculateConsistencyRatio(variantMatrix, consistencyIndex);
+
+if (!isConsistencyAcceptable(consistencyRatio)) {
+  const weightsRatioMatrix = calculateWeightsRatioMatrix(weightVector);
+  const weightsCorrectedRatioMatrix = calculateCorrectedWeightRatioMatrix(variantMatrix, weightsRatioMatrix);
+  const maxSumIndex = findMaxSumIndex(weightsCorrectedRatioMatrix);
+
+  variantMatrix = calculateNewCorrectedMatrix(variantMatrix, weightsRatioMatrix, maxSumIndex);
+
+  priorityVector = calculatePriorityVector(variantMatrix);
+  priorityVectorSum = priorityVector.reduce((acc, value) => acc + value, 0);
+  weightVector = calculateWeightVector(priorityVector);
+  matrixWeightVector = calculateMatrixWeightVector(variantMatrix);
+  lambdaVector = calculateLambdaVector(weightVector, matrixWeightVector);
+  eigenvalue = calculateEigenvalue(lambdaVector);
+  consistencyIndex = calculateConsistencyIndex(eigenvalue, lambdaVector);
+  consistencyRatio = calculateConsistencyRatio(variantMatrix, consistencyIndex);
+}
 
 console.log(
   `
